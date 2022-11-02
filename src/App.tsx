55e5eaee671beaf3580221a9834c58e9d61e78eb
da-in <talown@naver.com>
2022-11-02 20:16:05 +0900

    :sparkles: Create Pages
    Create Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Create from './pages/Create';
+import Home from './pages/Home';
+import Read from './pages/Read';
+import User from './pages/User';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edits <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:user" element={<User />} />
+      <Route path="/create/:user" element={<Create />} />
+      <Route path="/read/:no" element={<Read />} />
+    </Routes>
   );
 }
 
