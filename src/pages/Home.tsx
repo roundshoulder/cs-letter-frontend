0cd26973415a101b6e9da28ea9eed7d34caafad7
da-in <talown@naver.com>
2022-11-05 01:03:17 +0900

    :bug: delete oauth
    delete oauth

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { KAKAO_AUTH_URL } from '../api/oauth';
+// import { KAKAO_AUTH_URL } from '../api/oauth';
 
 function Home() {
   return (
     <div>
-      <a href={KAKAO_AUTH_URL}>
+      {/* <a href={KAKAO_AUTH_URL}>
         <button>카카오 로그인</button>
-      </a>
+      </a> */}
     </div>
   );
 }
