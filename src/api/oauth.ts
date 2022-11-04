6fc8b84a160bfcd20fd4923565625facbfac38d7
da-in <talown@naver.com>
2022-11-05 01:03:48 +0900

    :memo: create oauth
    create oauth

---
+++ src/api/oauth.ts
@@ -0,0 +1,5 @@
+import { baseURL } from './client';
+
+const REST_API_KEY = '12e53c031e79b63b32a7f4d5ee1fbcf2';
+const REDIRECT_URI = `${baseURL}/ouath`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
