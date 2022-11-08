018dce27f5b56da3965a7f6ea0f47ae65b0b91b9
da-in <talown@naver.com>
2022-11-09 03:50:52 +0900

    :recycle: Refactor Footer
    Refactor Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,30 +2,27 @@ import { css } from '@emotion/css';
 import { useLocation } from 'react-router-dom';
 import Theme from '../assets/Theme';
 
-const footer = css`
-  width: 100%;
-  height: 160px;
-  padding: 30px;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  background-color: #f8f8f8;
-  color: #757575;
-  position: absolute;
-  bottom: 0;
-`;
-
-const homeFooter = css`
-  background-color: #0a0a0a;
-  color: #fafafa;
-`;
-
 function Footer() {
-  const path = useLocation().pathname;
+  const isHome = useLocation().pathname === '/';
+  const footer = css`
+    width: 100%;
+    height: 160px;
+    padding: 30px;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    background-color: ${isHome ? '#0a0a0a' : '#f8f8f8'};
+    color: ${isHome ? '#fafafa' : '#757575'};
+    position: absolute;
+    bottom: 0;
+  `;
+
   return (
-    <div className={path !== '/' ? footer : `${footer} ${homeFooter}`}>
-      <span style={{ fontWeight: Theme.fontWeight.medium }}>ㅊㅅㅍㅈ</span>
+    <div className={footer}>
+      <span style={{ fontWeight: Theme.fontWeight.medium, fontSize: '16px' }}>
+        ㅊㅅㅍㅈ
+      </span>
       <div>
         <span>
           © 2022 Name all rights reserved.
