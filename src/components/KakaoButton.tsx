b87b3d38280f68dbc037517fc76a33c470c8534e
da-in <talown@naver.com>
2022-11-08 18:45:03 +0900

    :sparkles: Create Kakao Button
    Create Kakao Button

--- src/components/KakaoButton.tsx
+++ src/components/KakaoButton.tsx
@@ -1,14 +1,29 @@
 import { css } from '@emotion/css';
 import { KAKAO_AUTH_URL } from '../api/oauth';
+import kakao from '../assets/kakao.svg';
+import Theme from '../assets/Theme';
 
 const kakaoButton = css`
-  background-color: #000000;
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 47px;
+  border-radius: 12px;
+  border: none;
+  background-color: #fee500;
+  text-decoration: none;
+  font-weight: ${Theme.fontWeight.bold};
 `;
 
 function KakaoButton() {
   return (
     <a href={KAKAO_AUTH_URL}>
-      <button className={kakaoButton}>카카오 로그인</button>
+      <button className={kakaoButton}>
+        <img src={kakao} height="20" alt="kakao" />
+        카카오 로그인
+      </button>
     </a>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import { css } from '@emotion/css';
 import { KAKAO_AUTH_URL } from '../api/oauth';
+import KakaoButton from '../components/KakaoButton';
+
+const intro = css`
+  color: #fafafa;
+  padding: 26px;
+`;
 
 function Home() {
   return (
-    <div>
-      <a href={KAKAO_AUTH_URL}>
-        <button>카카오 로그인</button>
-      </a>
-    </div>
+    <>
+      <div className={intro}>초성편지를 보내보세요!</div>
+      <KakaoButton />
+    </>
   );
 }
 
