b34d2f421e61383ec4451fe34eeecf54635fdd81
da-in <talown@naver.com>
2022-11-09 13:56:34 +0900

    :sparkles: Apply React Query Provider
    Apply React Query Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import { Route, Routes } from 'react-router-dom';
 import MainContainer from './components/MainContainer';
 import KakaoRedirectHandler from './pages/KakaoRedirectHandler';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import Create from './pages/Create';
 import Home from './pages/Home';
 import Read from './pages/Read';
 import User from './pages/User';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <MainContainer>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:user" element={<User />} />
-        <Route path="/create/:user" element={<Create />} />
-        <Route path="/read/:no" element={<Read />} />
-        <Route path="/oauth/callback/*" element={<KakaoRedirectHandler />} />
-      </Routes>
-    </MainContainer>
+    <QueryClientProvider client={queryClient}>
+      <MainContainer>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:user" element={<User />} />
+          <Route path="/create/:user" element={<Create />} />
+          <Route path="/read/:no" element={<Read />} />
+          <Route path="/oauth/callback/*" element={<KakaoRedirectHandler />} />
+        </Routes>
+      </MainContainer>
+    </QueryClientProvider>
   );
 }
 
